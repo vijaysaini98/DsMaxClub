14fe539bca458967c6d70d1046d1bac626102c22
NSaiEPS <sainizameps@gmail.com>
2026-03-28 12:44:50 +0530

    issues fixing

--- src/components/CommonCard.tsx
+++ src/components/CommonCard.tsx
@@ -1,17 +1,37 @@
 import { StyleSheet, Image, View, Dimensions } from 'react-native';
 import React, { useRef } from 'react';
 import { colors } from '../theme/colors';
-import { AppText, BLACK, BOLD, BUTTON_TEXT, EIGHTEEN, FOURTEEN, MEDIUM, PLACEHOLDER, SEMI_BOLD, SIXTEEN, TEN, THIRD, TWELVE, WHITE } from './AppText';
+import {
+  AppText,
+  BLACK,
+  BOLD,
+  BUTTON_TEXT,
+  EIGHTEEN,
+  FOURTEEN,
+  MEDIUM,
+  PLACEHOLDER,
+  SEMI_BOLD,
+  SIXTEEN,
+  TEN,
+  THIRD,
+  TWELVE,
+  WHITE,
+} from './AppText';
 import { CardProps } from 'src/types/common';
 import TouchableOpacityView from './TouchableOpacityView';
-import { contactIcon, downArrowIcon, helpLineIcon, locationIcon, shareIcon } from '@helper/imagesAssets';
+import {
+  contactIcon,
+  downArrowIcon,
+  locationIcon,
+  shareIcon,
+} from '@helper/imagesAssets';
 import { ms, s, vs } from 'react-native-size-matters/extend';
 import RenderHTML from 'react-native-render-html';
 import MultiLocationSheet from '@screens/detail/ui/multiLoctionSheet';
 
-
 const { width } = Dimensions.get('window');
 const CommonCard = ({
+  hideViewButton,
   data,
   showRedeemBtn = false,
   onViewPress,
@@ -22,7 +42,8 @@ const CommonCard = ({
   btnStyle,
   btnTextColor,
   handleRightIcon,
-  heading, description,
+  heading,
+  description,
   price,
   actualPrice,
   location,
@@ -48,7 +69,11 @@ const CommonCard = ({
   return (
     <View style={styles.card}>
       <View style={styles.topRow}>
-        <AppText type={SIXTEEN} weight={SEMI_BOLD} style={{ width: status ? "80%" : "100%" }}>
+        <AppText
+          type={SIXTEEN}
+          weight={SEMI_BOLD}
+          style={{ width: status ? '80%' : '100%' }}
+        >
           {heading}
         </AppText>
 
@@ -57,54 +82,64 @@ const CommonCard = ({
             style={[
               styles.statusBadge,
               {
-                backgroundColor:
-                  statusBg ? statusBg :
-                    status === 'Active' ? colors.lightGreen : colors.tabBg,
+                backgroundColor: statusBg
+                  ? statusBg
+                  : status === 'Active'
+                  ? colors.lightGreen
+                  : colors.tabBg,
               },
-            ]}>
+            ]}
+          >
             <AppText
               type={TWELVE}
-              color={statusTextColor ? statusTextColor : status === 'Active' ? WHITE : BUTTON_TEXT}
+              color={
+                statusTextColor
+                  ? statusTextColor
+                  : status === 'Active'
+                  ? WHITE
+                  : BUTTON_TEXT
+              }
               weight={SEMI_BOLD}
-              style={{ textTransform: 'capitalize' }}>{status}</AppText>
+              style={{ textTransform: 'capitalize' }}
+            >
+              {status}
+            </AppText>
           </View>
         )}
-        {
-          rightIcon && (
-            <TouchableOpacityView
-              activeOpacity={handleRightIcon ? 0.8 : 1}
-              onPress={handleRightIcon}
-            >
-              <Image
-                source={shareIcon}
-                style={styles.shareIcon}
-                tintColor={colors.disTextColor}
-              />
-            </TouchableOpacityView>
-          )
-        }
+        {rightIcon && (
+          <TouchableOpacityView
+            activeOpacity={handleRightIcon ? 0.8 : 1}
+            onPress={handleRightIcon}
+          >
+            <Image
+              source={shareIcon}
+              style={styles.shareIcon}
+              tintColor={colors.disTextColor}
+            />
+          </TouchableOpacityView>
+        )}
       </View>
-      {/* {vendorName &&
-        <AppText weight={MEDIUM}>{vendorName}</AppText>
-      } */}
-      {/* {
-        shortDesc && (
-          <AppText type={TEN} weight={MEDIUM} color={PLACEHOLDER}>{shortDesc}</AppText>
-        )
-      } */}
-       <AppText style={{ width: status ? "80%" : "100%",marginVertical: vs(4) }} color={PLACEHOLDER} weight={BOLD}>
-          {couponCount ? `No. of Coupons: ${couponCount}` : null}
+      {vendorName && <AppText weight={MEDIUM}>{vendorName}</AppText>}
+      {shortDesc && (
+        <AppText type={TEN} weight={MEDIUM} color={PLACEHOLDER}>
+          {shortDesc}
         </AppText>
+      )}
+
+      <AppText
+        style={{ width: status ? '80%' : '100%', marginVertical: vs(4) }}
+        color={PLACEHOLDER}
+      >
+        {completeShortDesc ? completeShortDesc : null}
+      </AppText>
       <View style={styles.rowContainer}>
         {/* <View > */}
-        {htmlContent ?
-
-          (
-            <View style={{ marginVertical: vs(4) }}>
-              <RenderHTML
-                contentWidth={width}
-                source={{ html: htmlContent }}
-                baseStyle={{ color: colors.buttonText }}
+        {htmlContent ? (
+          <View style={{ marginVertical: vs(8) }}>
+            <RenderHTML
+              contentWidth={width}
+              source={{ html: htmlContent }}
+              baseStyle={{ color: colors.buttonText }}
               // tagsStyles={{
               //     h1: { fontSize: TWENTY_TW, fontWeight: 'bold', color: colors.black },
               //     h2: { fontSize: TWENTY, fontWeight: 'bold', color: colors.black },
@@ -114,22 +149,29 @@ const CommonCard = ({
               //     a: { color: 'blue', textDecorationLine: 'underline' },
               //     b: { fontWeight: 'bold' }
               // }}
-              />
-            </View>
-          ) :
-          (<AppText
+            />
+          </View>
+        ) : (
+          <AppText
             // numberOfLines={2}
-            style={styles.description} type={EIGHTEEN} weight={BOLD}>{description}</AppText>)
-        }
+            style={styles.description}
+            type={EIGHTEEN}
+            weight={BOLD}
+          >
+            {description}
+          </AppText>
+        )}
         {/* </View> */}
 
         {price && (
           <View style={styles.priceRow}>
-            <AppText weight={BOLD} type={FOURTEEN}
+            <AppText
+              weight={BOLD}
+              type={FOURTEEN}
               color={BLACK}
-              style={styles.price}>
+              style={styles.price}
+            >
               {`Rs. ${price}`}
-
             </AppText>
             {actualPrice && (
               <AppText weight={MEDIUM} style={styles.strikeThrough}>
@@ -139,88 +181,115 @@ const CommonCard = ({
           </View>
         )}
       </View>
-      {/* {couponCount && (
+      {couponCount && (
         <View style={styles.couponCountContainer}>
-          <AppText type={TEN}
-            weight={MEDIUM}
+          <AppText
+            type={TEN}
+            weight={BOLD}
             // color={WHITE}
             style={{ color: colors.placeholder2 }}
           >{`No of Coupons: ${couponCount}`}</AppText>
-
-        </View>)} */}
+        </View>
+      )}
+      {onContactPress && (
+        <TouchableOpacityView
+          style={{ paddingVertical: vs(4) }}
+          onPress={onContactPress}
+        >
+          <Image
+            source={contactIcon}
+            style={{
+              tintColor: colors.buttonBg,
+              width: s(30),
+              height: vs(30),
+              marginTop: vs(4),
+            }}
+          />
+        </TouchableOpacityView>
+      )}
 
       {usedCoupon && usedCoupon > 0 && (
         <View style={styles.usedCouponCountContainer}>
-          <AppText type={TEN}
+          <AppText
+            type={TEN}
             weight={MEDIUM}
             // color={WHITE}
             style={{ color: colors.placeholder2 }}
           >{`No of Used Coupons: ${usedCoupon}`}</AppText>
+        </View>
+      )}
 
-        </View>)}
+      {location && location?.length > 0 && (
+        <View
+          // onPress={()=> openInGoogleMaps(data?.client?.location_url)}
 
-      {
-        (location && location?.length > 0) && (
-          <View
-            // onPress={()=> openInGoogleMaps(data?.client?.location_url)}
-
-            style={{ flexDirection: 'row', gap: 2, alignItems: 'center' }}
-          >
-            {/* <TouchableOpacityView
+          style={{
+            flexDirection: 'row',
+            gap: 2,
+            alignItems: 'center',
+            marginTop: vs(10),
+          }}
+        >
+          {/* <TouchableOpacityView
               // onPress={() => handleRedirection(data?.locations[0]?.location_url)}
               style={{ flexDirection: 'row', gap: 5, alignItems: 'center' }}
             > */}
+          <Image
+            source={locationIcon}
+            style={{ height: vs(15), width: s(15) }}
+            tintColor={colors.borderColor}
+            resizeMode="contain"
+          />
+          <AppText
+            type={TWELVE}
+            color={BUTTON_TEXT}
+            numberOfLines={2}
+            style={{
+              letterSpacing: 0.8,
+              width: '90%',
+            }}
+          >
+            {location[0]?.location}
+          </AppText>
+          {/* </TouchableOpacityView> */}
+          <TouchableOpacityView onPress={() => sheetRef.current?.present()}>
             <Image
-              source={locationIcon}
-              style={{ height: vs(15), width: s(15) }}
-              tintColor={colors.borderColor}
-              resizeMode='contain'
+              source={downArrowIcon}
+              style={{ height: vs(15), width: s(15), marginTop: 3 }}
+              resizeMode="contain"
             />
-            <AppText
-
-              type={TWELVE} color={BUTTON_TEXT}
-              numberOfLines={2}
-              style={{
-                letterSpacing: 0.8,
-                width: "90%"
-              }} >{location[0]?.location}</AppText>
-            {/* </TouchableOpacityView> */}
-            <TouchableOpacityView
-              onPress={() => sheetRef.current?.present()}
-            >
-              <Image
-                source={downArrowIcon}
-                style={{ height: vs(15), width: s(15), marginTop: 3 }}
-                resizeMode='contain'
-              />
-            </TouchableOpacityView>
-          </View>
-        )
-      }
-
-        <AppText style={{ width: status ? "80%" : "100%",marginVertical: vs(4) }} color={PLACEHOLDER}>
-          {completeShortDesc ? completeShortDesc : null}
-        </AppText>
-<TouchableOpacityView style={{paddingVertical: vs(4)}} onPress={onContactPress}>
-  <Image source={contactIcon} style={{tintColor:colors.buttonBg, width: s(30), height: vs(30)}}/>
-</TouchableOpacityView>
+          </TouchableOpacityView>
+        </View>
+      )}
 
       <View style={styles.buttonRow}>
-        {/* <TouchableOpacityView
-          style={[showRedeemBtn ? styles.viewButton2 : styles.viewButton1(viewBtnDisabled), btnStyle]}
-          onPress={onViewPress}
-          disabled={viewBtnDisabled}
-          loader={viewBtnLoader}
-        >
-          <AppText
-            type={SIXTEEN}
-            weight={BOLD}
-            color={btnTextColor ? btnTextColor : showRedeemBtn ? colors.third : colors.white}
+        {!hideViewButton && (
+          <TouchableOpacityView
+            style={[
+              showRedeemBtn
+                ? styles.viewButton2
+                : styles.viewButton1(viewBtnDisabled),
+              btnStyle,
+            ]}
+            onPress={onViewPress}
+            disabled={viewBtnDisabled}
+            loader={viewBtnLoader}
           >
-            {buttonTitle ? buttonTitle : "VIEW"}
-          </AppText>
-        </TouchableOpacityView> */}
-       
+            <AppText
+              type={SIXTEEN}
+              weight={BOLD}
+              color={
+                btnTextColor
+                  ? btnTextColor
+                  : showRedeemBtn
+                  ? colors.third
+                  : colors.white
+              }
+            >
+              {buttonTitle ? buttonTitle : 'VIEW'}
+            </AppText>
+          </TouchableOpacityView>
+        )}
 
         {showRedeemBtn && (
           <TouchableOpacityView
@@ -229,7 +298,7 @@ const CommonCard = ({
             disabled={redeemDisabled}
           >
             <AppText type={SIXTEEN} weight={BOLD} style={styles.redeemText}>
-              {buttonTitle2 ? buttonTitle2 : "REDEEM"}
+              {buttonTitle2 ? buttonTitle2 : 'REDEEM'}
             </AppText>
           </TouchableOpacityView>
         )}
@@ -251,8 +320,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     borderColor: colors.black,
     borderWidth: 2,
-    borderStyle: "dotted"
-
+    borderStyle: 'dotted',
   },
   topRow: {
     flexDirection: 'row',
@@ -265,7 +333,7 @@ const styles = StyleSheet.create({
     paddingVertical: 4,
     position: 'absolute',
     right: -5,
-    top: -10
+    top: -10,
   },
   statusText: {
     // color: colors.white,
@@ -275,12 +343,12 @@ const styles = StyleSheet.create({
     // marginVertical: vs(8),
     marginVertical: vs(8),
     // width: "90%",
-    letterSpacing: (0.5),
+    letterSpacing: 0.5,
   },
   priceRow: {
     flexDirection: 'row',
     alignItems: 'flex-end',
-    gap: ms(5)
+    gap: ms(5),
   },
   price: {
     // marginRight: 8,
@@ -292,7 +360,7 @@ const styles = StyleSheet.create({
   buttonRow: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    gap: ms(10)
+    gap: ms(10),
   },
   viewButton1: (disabledBtn: boolean) => ({
     marginTop: vs(20),
@@ -322,14 +390,14 @@ const styles = StyleSheet.create({
     paddingVertical: vs(10),
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   redeemText: {
     color: colors.white,
   },
   shareIcon: {
     width: s(20),
-    height: vs(20)
+    height: vs(20),
   },
   rowContainer: {
     flexDirection: 'row',
@@ -342,7 +410,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.tabBg,
     maxWidth: s(120),
     padding: ms(10),
-    marginTop: vs(10)
+    marginTop: vs(10),
   },
   usedCouponCountContainer: {
     borderWidth: 0.5,
@@ -351,8 +419,8 @@ const styles = StyleSheet.create({
     backgroundColor: colors.mainBg,
     maxWidth: s(150),
     padding: ms(10),
-    marginVertical: vs(5)
-  }
+    marginVertical: vs(5),
+  },
 });
 
 export default CommonCard;
--- src/screens/detail/ui/all.tsx
+++ src/screens/detail/ui/all.tsx
@@ -1,5 +1,11 @@
 import React, { useMemo, useCallback, useRef, useState } from 'react';
-import { Animated, Keyboard, RefreshControl, StyleSheet, View } from 'react-native';
+import {
+  Animated,
+  Keyboard,
+  RefreshControl,
+  StyleSheet,
+  View,
+} from 'react-native';
 import CommonCard from '@components/CommonCard';
 import { colors } from '@theme/colors';
 import { shareToAny } from '@utils/index';
@@ -7,144 +13,147 @@ import { useAppSelector, useAppDispatch } from '@redux/hooks';
 import { Loader } from '@components/Spinner';
 import { WHITE } from '@components/AppText';
 import ViewDetailsBottomSheet from './viewDetailsBottomSheet';
-import { getBookletDetail, getComboBookletDetail } from '@actions/home/homeAction';
+import {
+  getBookletDetail,
+  getComboBookletDetail,
+} from '@actions/home/homeAction';
 import { ms, s, vs } from 'react-native-size-matters/extend';
 import ListEmptyComponent from '@components/ListEmptyComponent';
 import CouponsShimerLoader from '@components/ShimerLoader/CouponsShimerLoader';
-import PhoneDialerModal from '@screens/helpLine/contactModal';
 import VendorPhoneDialerModal from './vendorContactModal';
 
 interface CardItem {
-    id: string | number;
-    // Add other properties as needed
+  id: string | number;
+  // Add other properties as needed
 }
 
-const All: React.FC = ({ id, from, scrollY,handleViewPress }) => {
-    const dispatch = useAppDispatch();
-    const { bookletDetailAllDeals, isLoading } = useAppSelector((state) => state?.home);
+const All: React.FC = ({ id, from, scrollY, handleViewPress }) => {
+  const dispatch = useAppDispatch();
+  const { bookletDetailAllDeals, isLoading } = useAppSelector(
+    state => state?.home,
+  );
 
-    const [couponDetail, setCouponDetail] = useState<any>();
-    const [refreshing, setRefreshing] = useState(false);
-    const viewDetailSheet = useRef<any>(null);
-    const executiveSnapPoints = useMemo(() => ["50%", "80%"], []);
-    const [isPhoneDialerModalVisible, setIsPhoneDialerModalVisible] = React.useState(false);
-    const [selectedVendor, setSelectedVendor] = useState<any>(null);
-    
+  const [couponDetail, setCouponDetail] = useState<any>();
+  const [refreshing, setRefreshing] = useState(false);
+  const viewDetailSheet = useRef<any>(null);
+  const executiveSnapPoints = useMemo(() => ['50%', '80%'], []);
+  const [isPhoneDialerModalVisible, setIsPhoneDialerModalVisible] =
+    React.useState(false);
+  const [selectedVendor, setSelectedVendor] = useState<any>(null);
 
-    const onViewPress = useCallback((item: CardItem) => {
-        // setCouponDetail(item);
-        // setTimeout(() => {
-        //     // viewDetailSheet?.current?.open();
-        //     viewDetailSheet?.current?.expand()
-        // }, 200);
-        handleViewPress(item)
-    }, []);
+  const onViewPress = useCallback((item: CardItem) => {
+    // setCouponDetail(item);
+    // setTimeout(() => {
+    //     // viewDetailSheet?.current?.open();
+    //     viewDetailSheet?.current?.expand()
+    // }, 200);
+    handleViewPress(item);
+  }, []);
 
-    const handleShareOnPress = useCallback((item: CardItem) => {
-        shareToAny('hello');
-    }, []);
+  const handleShareOnPress = useCallback((item: CardItem) => {
+    shareToAny('hello');
+  }, []);
 
-    const onRefresh = useCallback(() => {
-        setRefreshing(true);
-        let data = {
-            booklet_id: id,
-            tabname: "All Deals"
-        }
-        if (from == "ComboBooklet") {
-            dispatch(getComboBookletDetail(data)).finally(() => setRefreshing(false));
-        } else {
-            dispatch(getBookletDetail(data)).finally(() => setRefreshing(false));
-        }
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    let data = {
+      booklet_id: id,
+      tabname: 'All Deals',
+    };
+    if (from == 'ComboBooklet') {
+      dispatch(getComboBookletDetail(data)).finally(() => setRefreshing(false));
+    } else {
+      dispatch(getBookletDetail(data)).finally(() => setRefreshing(false));
+    }
+  }, [dispatch]);
 
-    }, [dispatch]);
+  const renderItem = useMemo(
+    () =>
+      ({ item }: { item: CardItem }) => {
+        console.log(item, 'card items');
 
-    const renderItem = useMemo(
-        () =>
-            ({ item }: { item: CardItem }) => {
-                console.log(item,'item of card====>');
-                
-                return (
-                    <CommonCard
-                    couponCount={item?.no_of_coupons}
-                        data={item}
-                        heading={item?.heading}
-                        // description={item?.short_desc}
-                        htmlContent={item?.description}
-                        onViewPress={() => onViewPress(item)}
-                        btnStyle={styles.viewBtnStyle}
-                        status={item?.coupon_type_id == 1 ? 'Free' : ""}
-                        statusBg={item?.coupon_type_id == 1 && colors.buttonBg}
-                        statusTextColor={item?.coupon_type_id == 1 && WHITE}
-                        location={from == "ComboBooklet" ? item?.locations : null}
-                        // vendorName={item?.vendor?.name}
-                        shortDesc={item?.vendor?.short_desc}
-                        completeShortDesc={item?.short_desc}
-                        onContactPress={() => {
-    setSelectedVendor(item);
-    setIsPhoneDialerModalVisible(true);
-}}
-                    />
-                )
-            },
-        [onViewPress, handleShareOnPress]
-    );
+        return (
+          <CommonCard
+            data={item}
+            heading={item?.heading}
+            // description={item?.short_desc}
+            htmlContent={item?.description}
+            onViewPress={() => onViewPress(item)}
+            btnStyle={styles.viewBtnStyle}
+            status={item?.coupon_type_id == 1 ? 'Free' : ''}
+            statusBg={item?.coupon_type_id == 1 && colors.buttonBg}
+            statusTextColor={item?.coupon_type_id == 1 && WHITE}
+            location={from == 'ComboBooklet' ? item?.locations : null}
+            vendorName={item?.vendor?.name}
+            // shortDesc={item?.vendor?.short_desc}
+            hideViewButton={true}
+            couponCount={item?.no_of_coupons}
+            completeShortDesc={item?.short_desc}
+            onContactPress={() => {
+              setSelectedVendor(item);
+              setIsPhoneDialerModalVisible(true);
+            }}
+          />
+        );
+      },
+    [onViewPress, handleShareOnPress],
+  );
 
-   
-    return (
-        <View style={{ flex: 1 }}>
-            {isLoading ? (
-                // <Loader />
-                <CouponsShimerLoader />
-            ) : (
-                <Animated.FlatList
-                    data={bookletDetailAllDeals?.coupons}
-                    renderItem={renderItem}
-                    keyExtractor={item => item.id.toString()}
-                    contentContainerStyle={styles.containerStyle}
-                    showsVerticalScrollIndicator={false}
-                    refreshControl={
-                        <RefreshControl
-                            refreshing={refreshing}
-                            onRefresh={onRefresh}
-                            colors={[colors.buttonBg]}
-                            tintColor={colors.buttonBg}
-                        />
-                    }
-                    ListEmptyComponent={() => (
-                        <ListEmptyComponent title={"No Coupons Available"} />
-                    )}
-                    onScroll={Animated.event(
-                        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-                        { useNativeDriver: false }
-                    )}
-                    scrollEventThrottle={16}
-                />
-            )}
-            {/* <ViewDetailsBottomSheet
+  return (
+    <View style={{ flex: 1 }}>
+      {isLoading ? (
+        // <Loader />
+        <CouponsShimerLoader />
+      ) : (
+        <Animated.FlatList
+          data={bookletDetailAllDeals?.coupons}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+          contentContainerStyle={styles.containerStyle}
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              colors={[colors.buttonBg]}
+              tintColor={colors.buttonBg}
+            />
+          }
+          ListEmptyComponent={() => (
+            <ListEmptyComponent title={'No Coupons Available'} />
+          )}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            { useNativeDriver: false },
+          )}
+          scrollEventThrottle={16}
+        />
+      )}
+      {/* <ViewDetailsBottomSheet
                 data={couponDetail}
                 ref={viewDetailSheet}
             /> */}
-          <VendorPhoneDialerModal
-    visible={isPhoneDialerModalVisible}
-    vendor={selectedVendor}
-    onClose={() => setIsPhoneDialerModalVisible(false)}
-/>
-        </View>
-    );
+      <VendorPhoneDialerModal
+        visible={isPhoneDialerModalVisible}
+        vendor={selectedVendor}
+        onClose={() => setIsPhoneDialerModalVisible(false)}
+      />
+    </View>
+  );
 };
 
 export default All;
 
 const styles = StyleSheet.create({
-    containerStyle: {
-        gap: s(15),
-        paddingBottom: vs(100),
-        marginTop: vs(15),
-    },
-    viewBtnStyle: {
-        borderWidth: 1,
-        borderRadius: ms(100),
-        borderColor: colors.placeholder2,
-        backgroundColor: colors.white,
-    },
+  containerStyle: {
+    gap: s(15),
+    paddingBottom: vs(100),
+    marginTop: vs(15),
+  },
+  viewBtnStyle: {
+    borderWidth: 1,
+    borderRadius: ms(100),
+    borderColor: colors.placeholder2,
+    backgroundColor: colors.white,
+  },
 });
