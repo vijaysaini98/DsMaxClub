332490afbea9e643e08d55f5dac3ecde6337eda9
Priyanka19-04 <124132784+Priyanka19-04@users.noreply.github.com>
2026-04-06 15:53:26 +0530

    package name change

--- android/app/src/main/java/com/dsmaxclub/MainActivity.kt
+++ android/app/src/main/java/com/dsmaxclub/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dsmaxclub
+package com.dsmaxclubdemo
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/dsmaxclub/MainApplication.kt
+++ android/app/src/main/java/com/dsmaxclub/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.dsmaxclub
+package com.dsmaxclubdemo
 
 import android.app.Application
 import com.facebook.react.PackageList
